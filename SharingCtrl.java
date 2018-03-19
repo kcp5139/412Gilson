088d6d1de0b12fed65d352c246d59c34bde32028
kcp5139 <kcp5139@psu.edu>
2018-03-18 20:26:01 -0400

    Add files via upload

---
+++ SharingCtrl.java
@@ -0,0 +1,46 @@
+package socialmediausecase;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kcp5139
+ */
+public class SharingCtrl {
+    
+    SharingCtrl(){
+        
+    }
+    
+    String basicData;
+    
+    
+    /**
+     * Gets the data collected from the individual runs
+     */
+    public String getBasicData(String basicRunData){
+        
+        
+        return basicData;
+    }
+    
+    /**
+     * Organizes the data into the user's desired format
+     */ 
+    public void organizeData(String basicData){
+       
+        
+    }
+    
+    /**
+     * Redirects the user to the chosen social media site and creates a post for them to confirm
+     */
+     public void shareData(){
+         
+     }
+     
+}
